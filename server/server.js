6fb02a7cfbc76edc42954be7245f6c0520a31e8b
Ujjval <ujjvalkumaria@gmail.com>
2017-11-03 01:17:32 -0700

    added json web token

--- server/server.js
+++ server/server.js
@@ -112,11 +112,13 @@ app.post('/users', (req, res) => {
 
     var user = new User(body);
 
-    user.save().then((user) => {
-        res.send(user);
-    }, (err) => {
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
         res.status(400).send(err);
-    })
-})
+    });
+});
 
 app.listen(3000);
