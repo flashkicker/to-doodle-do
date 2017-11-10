288d8aa8e7114acc3445616a32ca97b63913605f
Ujjval <ujjvalkumaria@gmail.com>
2017-11-10 13:14:19 -0800

    added logout route

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,18 @@ UserSchema.methods.generateAuthToken = function() {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+}
+
 UserSchema.statics.findByToken = function(token) {
     var User = this;
     var decoded;
@@ -71,14 +83,14 @@ UserSchema.statics.findByToken = function(token) {
 
 UserSchema.statics.findByCredentials = function (email, password) {
     var User = this;
-
+    
     return User.findOne({
         email
     }).then((user) => {
         if(!user) {
             return Promise.reject();
         }
-
+        
         return new Promise((resolve, reject) => {
             bcrypt.compare(password, user.password, (err, res) => {
                 if(res) {
@@ -94,7 +106,7 @@ UserSchema.statics.findByCredentials = function (email, password) {
 
 UserSchema.pre('save', function (next) {
     var user = this;
-
+    
     if(user.isModified('password')) {
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(user.password, salt, (err, hash) => {
